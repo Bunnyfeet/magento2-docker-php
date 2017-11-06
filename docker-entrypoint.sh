b9e7c6e706f2f93a597675eb795aff7d61c459f8
Mikko Ropanen <mikko.ropanen@digia.com>
2017-11-06 14:48:01 +0200

    Modified docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,10 +2,10 @@
 
 echo "Updating www-data uid and gid"
 
-MAGENTO_ROOT=/var/www/html
+WWW_ROOT=/var/www/html
 
-DOCKER_UID=`stat -c "%u" $MAGENTO_ROOT`
-DOCKER_GID=`stat -c "%g" $MAGENTO_ROOT`
+DOCKER_UID=`stat -c "%u" $WWW_ROOT`
+DOCKER_GID=`stat -c "%g" $WWW_ROOT`
 
 INCUMBENT_USER=`getent passwd $DOCKER_UID | cut -d: -f1`
 INCUMBENT_GROUP=`getent group $DOCKER_GID | cut -d: -f1`
@@ -16,12 +16,12 @@ echo "Incumbent: user = $INCUMBENT_USER, group = $INCUMBENT_GROUP"
 # Once we've established the ids and incumbent ids then we need to free them
 # up (if necessary) and then make the change to www-data.
 
-[ ! -z "${INCUMBENT_USER}" ] && usermod -u 99$DOCKER_UID $INCUMBENT_USER
-usermod -u $DOCKER_UID www-data
+#[ ! -z "${INCUMBENT_USER}" ] && usermod -u 99$DOCKER_UID $INCUMBENT_USER
+#usermod -u $DOCKER_UID www-data
 
-[ ! -z "${INCUMBENT_GROUP}" ] && groupmod -g 99$DOCKER_GID $INCUMBENT_GROUP
-groupmod -g $DOCKER_GID www-data
+#[ ! -z "${INCUMBENT_GROUP}" ] && groupmod -g 99$DOCKER_GID $INCUMBENT_GROUP
+#groupmod -g $DOCKER_GID www-data
 
-chown www-data:www-data $MAGENTO_ROOT
-
-exec "$@"
+#cd /var/www/html
+#find . -type d -exec chmod 700 {} \; && find . -type f -exec chmod 600 {} \;
+php-fpm -R -F
